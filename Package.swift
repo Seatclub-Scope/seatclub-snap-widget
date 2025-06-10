094cb7a0d2fc29e475a7c25ce9d4286b7e629963
Taylor Conlin <taylor@scopelabs.com>
2025-06-10 10:24:49 -0400

    Update Package.swift with release URL and checksum

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SeatclubApp",
-            url: "https://github.com/Seatclub-Scope/seatclub-widget/releases/download/1.0.0/SeatclubApp.xcframework.zip",
-            checksum: "your-checksum-here"
+            url: "https://github.com/Seatclub-Scope/seatclub-snap-widget/releases/download/1.0.0/SeatclubApp.xcframework.zip",
+            checksum: "820770af5c2780110cfc45729445dd3ba0904fc13eb6563f693f91f47a222b90"
         )
     ]
 )
